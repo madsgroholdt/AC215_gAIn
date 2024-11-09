b0cc96a299782ae05b35592d84927cfe548039f9
Mads Groeholdt <madsgroeholdt@college.harvard.edu>
2024-11-09 17:19:08 -0500

    Changed test function to different function

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,16 +1,60 @@
 import pytest
-from src.data_scraping import scrape
-import requests
-import os
+from typing import List
+from src.models.semantic_splitter import combine_sentences
 
 
-def test_scrape_real_site():
-    url = 'https://wholebrainhealth.org/exercise-and-brain-health/?gad_source=1&gclid=Cj0KCQiArby5BhCDARIsAIJvjIRzQvo-9defVqZe7r-aZXOCUjeN3xAeXGwdfFqsYQL3wmAF3Xaq0fIaAiO7EALw_wcB'
-    os.mkdir('articles')
-    assert scrape.get_article_content(url, 'test_file') is None
+def test_combine_sentences_with_default_buffer():
+    sentences = [
+        {"sentence": "Hello"},
+        {"sentence": "world"},
+        {"sentence": "from"},
+        {"sentence": "pytest"},
+    ]
+
+    expected_output = [
+        {"sentence": "Hello", "combined_sentence": "Hello world"},
+        {"sentence": "world", "combined_sentence": "Hello world from"},
+        {"sentence": "from", "combined_sentence": "world from pytest"},
+        {"sentence": "pytest", "combined_sentence": "from pytest"},
+    ]
+    result = combine_sentences(sentences)
+    assert result == expected_output
 
 
-def test_scrape_fake_site():
-    url = 'thisisafakeurl'
-    with pytest.raises(requests.exceptions.RequestException) as exception:
-        scrape.get_article_content(url, 'test_file')
+def test_combine_sentences_with_empty_input():
+    sentences = []
+
+    expected_output = []
+
+    result = combine_sentences(sentences)
+    assert result == expected_output
+
+
+def test_combine_sentences_with_single_sentence():
+    sentences = [{"sentence": "Single"}]
+
+    expected_output = [{"sentence": "Single", "combined_sentence": "Single"}]
+
+    result = combine_sentences(sentences)
+    assert result == expected_output
+
+
+# def test_combine_sentences_with_custom_buffer():
+#     sentences = [
+#         {"sentence": "The"},
+#         {"sentence": "quick"},
+#         {"sentence": "brown"},
+#         {"sentence": "fox"},
+#         {"sentence": "jumps"},
+#     ]
+
+#     expected_output = [
+#         {"sentence": "The", "combined_sentence": "The quick brown"},
+#         {"sentence": "quick", "combined_sentence": "The quick brown fox"},
+#         {"sentence": "brown", "combined_sentence": "The quick brown fox jumps"},
+#         {"sentence": "fox", "combined_sentence": "quick brown fox jumps"},
+#         {"sentence": "jumps", "combined_sentence": "brown fox jumps"},
+#     ]
+
+#     result = combine_sentences(sentences, buffer_size=2)
+#     assert result == expected_output
