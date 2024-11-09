d10ced61d9f1ff404f74bf3f3d9b64b4925e7fbc
Mads Groeholdt <madsgroeholdt@college.harvard.edu>
2024-11-09 16:53:36 -0500

    Added articles folder

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,10 +1,12 @@
 import pytest
 from src.data_scraping import scrape
 import requests
+import os
 
 
 def test_scrape_real_site():
     url = 'https://wholebrainhealth.org/exercise-and-brain-health/?gad_source=1&gclid=Cj0KCQiArby5BhCDARIsAIJvjIRzQvo-9defVqZe7r-aZXOCUjeN3xAeXGwdfFqsYQL3wmAF3Xaq0fIaAiO7EALw_wcB'
+    os.mkdir('articles')
     assert scrape.get_article_content(url, 'test_file') is None
 
 
