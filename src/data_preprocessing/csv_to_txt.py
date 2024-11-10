111f67e1a4e8fbd9d2eb47d402e5f31e7da5d765
Mads Groeholdt <madsgroeholdt@college.harvard.edu>
2024-11-10 13:56:10 -0500

    formatting fixes to make flake8 compliant

--- src/data_preprocessing/csv_to_txt.py
+++ src/data_preprocessing/csv_to_txt.py
@@ -1,25 +1,28 @@
 import os
-from pathlib import Path
 import string
 import pandas as pd
-from strava_api import get_strava_data
 
 
 def csv_to_txt(input_csv, output_txt):
     df = pd.read_csv(input_csv)
 
-    with open(output_txt, 'w') as f:
+    with open(output_txt, "w") as f:
         for _, row in df.iterrows():
             date = row.iloc[0]  # first column is 'date'
             date = pd.to_datetime(date)
             formatted_date = date.strftime("%A, %B %d")
-            day_suffix = "th" if 11 <= date.day <= 13 else {
-                1: "st", 2: "nd", 3: "rd"}.get(date.day % 10, "th")
+            day_suffix = (
+                "th"
+                if 11 <= date.day <= 13
+                else {1: "st", 2: "nd", 3: "rd"}.get(date.day % 10, "th")
+            )
             formatted_date += f"{day_suffix} {date.year}"
 
             # header for each entry
             f.write(
-                f"On {formatted_date}, I had the following health and activity metrics:\n")
+                f"On {formatted_date}, I had the following health and activity "
+                "metrics:\n"
+            )
 
             # get metric values
             for title in df.columns[1:]:
@@ -27,11 +30,11 @@ def csv_to_txt(input_csv, output_txt):
                 if pd.isna(value):
                     continue
 
-                left_p = title.find('(')
-                right_p = title.find(')')
-                metric = ''
+                left_p = title.find("(")
+                right_p = title.find(")")
+                metric = ""
                 if left_p != right_p and left_p != -1:
-                    metric = title[left_p+1:right_p]
+                    metric = title[left_p + 1: right_p]
 
                 if metric:
                     f.write(f"{string.capwords(title)} was {value} {metric}\n")
@@ -43,13 +46,14 @@ def csv_to_txt(input_csv, output_txt):
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    csv_folder_path = '/csv_data/'
-    txt_folder_path = '/txt_data/'
+    csv_folder_path = "/csv_data/"
+    txt_folder_path = "/txt_data/"
     for filename in os.listdir(cwd + csv_folder_path):
-        if filename.endswith('.csv'):
+        if filename.endswith(".csv"):
             csv_file = os.path.join(cwd + csv_folder_path, filename)
             txt_file = os.path.join(
-                cwd + txt_folder_path, filename.replace('.csv', '.txt'))
+                cwd + txt_folder_path, filename.replace(".csv", ".txt")
+            )
 
             csv_to_txt(csv_file, txt_file)
             print(f"Created txt file for {filename}")
--- src/models/model_rag.py
+++ src/models/model_rag.py
@@ -1,23 +1,25 @@
 import os
 import argparse
-from pydoc import doc
 import pandas as pd
-import json
-import time
 import glob
 import hashlib
 import chromadb
 from google.cloud import storage
-import shutil
 
 # Vertex AI
 import vertexai
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
-from vertexai.generative_models import GenerativeModel, GenerationConfig, Content, Part, ToolConfig
+from vertexai.generative_models import (
+    GenerativeModel,
+    GenerationConfig,
+    Content,
+    Part,
+    ToolConfig,
+)
 
 # Langchain
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 # from langchain_experimental.text_splitter import SemanticChunker
 from semantic_splitter import SemanticChunker
 import rag_agent_tools
@@ -45,29 +47,59 @@ generation_config = {
     "top_p": 0.95,  # Use nucleus sampling
 }
 # Initialize the GenerativeModel with specific system instructions
-SYSTEM_INSTRUCTION = """
-You are an AI assistant with expert knowledge about everything related to health and fitness. In your responses, you should leverage the information in the provided documents for anything related to questions about historical events, such as "what was my step count on date x". Ensure that you do not state claims as factual without being able to refer to the specific texts where you found said claims. If you are making assumptions or reasonings about health and fitness advice, ensure to state that this is only your "professional" opinion, and not necessarily advice that should be followed directly without consideration. Beyond the documents with historical information about the user's health and fitness data, state that you are giving qualitative advice based on your experiences/acquired knowledge, not advice directly based on documents you have access to.
-
-When answering a query:
-1. Carefully read all the text chunks provided and provide information about the historical activity data if the user is asking for it.
-2. Identify the most relevant information from these chunks to address the user's question if they are asking about their historical activities.
-3. Try to leverage the information from the provided chunks when giving the user advice. Use the information about their historical activity and health levels to provide context-aware responses to their questions.
-4. Always maintain a professional and knowledgeable tone, befitting a succesful, well-educated Personal Trainer, Physiotherapist, or other relevant health professional that fits the user's needs.
-
-Lastly, a few important points to remember:
-- You are an expert in health and fitness and should leverage all the knowledge you have, but should try your best to leverage the information provided in the chunks of text to personalize the responses to the user's historical levels of health and fitness.
-- If asked about topics unrelated to health and fitness, politely redirect the conversation back to subjects related to health and fitness.
-- Be concise in your responses while ensuring you cover all the relevant elements the user is asking about.
-
-Your goal is to provide accurate, helpful information about health and fitness to the user, by combining context from the provided chunks with your abundance of knowledge of the field.
-"""
+SYSTEM_INSTRUCTION = (
+    "You are an AI assistant with expert knowledge about everything "
+    "related to health and fitness. In your responses, you should "
+    "leverage the information in the provided documents for anything "
+    'related to questions about historical events, such as "what was '
+    'my step count on date x". Ensure that you do not state claims as '
+    "factual without being able to refer to the specific texts where "
+    "you found said claims. If you are making assumptions or reasonings "
+    "about health and fitness advice, ensure to state that this is only "
+    'your "professional" opinion, and not necessarily advice that should '
+    "be followed directly without consideration. Beyond the documents "
+    "with historical information about the user's health and fitness "
+    "data, state that you are giving qualitative advice based on your "
+    "experiences/acquired knowledge, not advice directly based on "
+    "documents you have access to.\n\n"
+    "When answering a query:\n"
+    "1. Carefully read all the text chunks provided and provide "
+    "information about the historical activity data if the user is "
+    "asking for it.\n"
+    "2. Identify the most relevant information from these chunks to "
+    "address the user's question if they are asking about their "
+    "historical activities.\n"
+    "3. Try to leverage the information from the provided chunks when "
+    "giving the user advice. Use the information about their historical "
+    "activity and health levels to provide context-aware responses to "
+    "their questions.\n"
+    "4. Always maintain a professional and knowledgeable tone, befitting "
+    "a successful, well-educated Personal Trainer, Physiotherapist, or "
+    "other relevant health professional that fits the user's needs.\n\n"
+    "Lastly, a few important points to remember:\n"
+    "- You are an expert in health and fitness and should leverage all "
+    "the knowledge you have, but should try your best to leverage the "
+    "information provided in the chunks of text to personalize the "
+    "responses to the user's historical levels of health and fitness.\n"
+    "- If asked about topics unrelated to health and fitness, politely "
+    "redirect the conversation back to subjects related to health and "
+    "fitness.\n"
+    "- Be concise in your responses while ensuring you cover all the "
+    "relevant elements the user is asking about.\n\n"
+    "Your goal is to provide accurate, helpful information about health "
+    "and fitness to the user, by combining context from the provided "
+    "chunks with your abundance of knowledge of the field."
+)
 generative_model = GenerativeModel(
-    GENERATIVE_MODEL,
-    system_instruction=[SYSTEM_INSTRUCTION]
+    GENERATIVE_MODEL, system_instruction=[SYSTEM_INSTRUCTION]
 )
 
 document_mappings = {
-    "Tomas Arevalo-2": {"type": "Activity Log", "source": "Apple Health", "user": "Tomas Arevalo"}
+    "Tomas Arevalo-2": {
+        "type": "Activity Log",
+        "source": "Apple Health",
+        "user": "Tomas Arevalo",
+    }
 }
 
 
@@ -86,20 +118,23 @@ def download_from_gcs(folder_name, local_path):
 
 
 def generate_query_embedding(query):
-    query_embedding_inputs = [TextEmbeddingInput(
-        task_type='RETRIEVAL_DOCUMENT', text=query)]
-    kwargs = dict(
-        output_dimensionality=EMBEDDING_DIMENSION) if EMBEDDING_DIMENSION else {}
+    query_embedding_inputs = [
+        TextEmbeddingInput(task_type="RETRIEVAL_DOCUMENT", text=query)
+    ]
+    kwargs = (
+        dict(output_dimensionality=EMBEDDING_DIMENSION) if EMBEDDING_DIMENSION else {}
+    )
     embeddings = embedding_model.get_embeddings(
         query_embedding_inputs, **kwargs)
     return embeddings[0].values
 
 
-def generate_text_embeddings(chunks, dimensionality: int = 256, batch_size=250):
+def generate_text_embeddings(chunks, dimensionality: int = 256,
+                             batch_size=250):
     # Max batch size is 250 for Vertex AI
     all_embeddings = []
     for i in range(0, len(chunks), batch_size):
-        batch = chunks[i:i+batch_size]
+        batch = chunks[i: i + batch_size]
         inputs = [TextEmbeddingInput(text, "RETRIEVAL_DOCUMENT")
                   for text in batch]
         kwargs = dict(
@@ -111,16 +146,14 @@ def generate_text_embeddings(chunks, dimensionality: int = 256, batch_size=250):
 
 
 def load_text_embeddings(df, collection, batch_size=500):
-
     # Generate ids
     df["id"] = df.index.astype(str)
     hashed_doc_names = df["doc_name"].apply(
-        lambda x: hashlib.sha256(x.encode()).hexdigest()[:16])
+        lambda x: hashlib.sha256(x.encode()).hexdigest()[:16]
+    )
     df["id"] = hashed_doc_names + "-" + df["id"]
 
-    metadata = {
-        "doc_name": df["doc_name"].tolist()[0]
-    }
+    metadata = {"doc_name": df["doc_name"].tolist()[0]}
     if metadata["doc_name"] in document_mappings:
         document_mapping = document_mappings[metadata["doc_name"]]
         metadata["type"] = document_mapping["type"]
@@ -131,7 +164,7 @@ def load_text_embeddings(df, collection, batch_size=500):
     total_inserted = 0
     for i in range(0, df.shape[0], batch_size):
         # Create a copy of the batch and reset the index
-        batch = df.iloc[i:i+batch_size].copy().reset_index(drop=True)
+        batch = df.iloc[i: i + batch_size].copy().reset_index(drop=True)
 
         ids = batch["id"].tolist()
         documents = batch["chunk"].tolist()
@@ -139,22 +172,21 @@ def load_text_embeddings(df, collection, batch_size=500):
         embeddings = batch["embedding"].tolist()
 
         collection.add(
-            ids=ids,
-            documents=documents,
-            metadatas=metadatas,
-            embeddings=embeddings
+            ids=ids, documents=documents, metadatas=metadatas, embeddings=embeddings
         )
         total_inserted += len(batch)
         print(f"Inserted {total_inserted} items...")
 
     print(
-        f"Finished inserting {total_inserted} items into collection '{collection.name}'")
+        f"Finished inserting {total_inserted} items into collection '{collection.name}'"
+    )
 
 
 def get_collection(method="recursive-split"):
     # Connect to chroma DB
     client = chromadb.HttpClient(host=CHROMADB_HOST, port=CHROMADB_PORT)
-    # Get a collection object from an existing collection, by name. If it doesn't exist, create it.
+    # Get a collection object from an existing collection, by name.
+    # If it doesn't exist, create it.
     collection_name = f"{method}-collection"
     # Get the collection
     collection = client.get_collection(name=collection_name)
@@ -214,9 +246,10 @@ def chunk(method="recursive-split"):
             print(data_df.head())
 
             jsonl_filename = os.path.join(
-                OUTPUT_FOLDER, f"chunks-{method}-{doc_name}.jsonl")
+                OUTPUT_FOLDER, f"chunks-{method}-{doc_name}.jsonl"
+            )
             with open(jsonl_filename, "w") as json_file:
-                json_file.write(data_df.to_json(orient='records', lines=True))
+                json_file.write(data_df.to_json(orient="records", lines=True))
 
 
 def embed(method="recursive-split"):
@@ -238,10 +271,12 @@ def embed(method="recursive-split"):
         chunks = data_df["chunk"].values
         if method == "semantic-split":
             embeddings = generate_text_embeddings(
-                chunks, EMBEDDING_DIMENSION, batch_size=15)
+                chunks, EMBEDDING_DIMENSION, batch_size=15
+            )
         else:
             embeddings = generate_text_embeddings(
-                chunks, EMBEDDING_DIMENSION, batch_size=100)
+                chunks, EMBEDDING_DIMENSION, batch_size=100
+            )
         data_df["embedding"] = embeddings
 
         # Save
@@ -250,7 +285,7 @@ def embed(method="recursive-split"):
 
         jsonl_filename = jsonl_file.replace("chunks-", "embeddings-")
         with open(jsonl_filename, "w") as json_file:
-            json_file.write(data_df.to_json(orient='records', lines=True))
+            json_file.write(data_df.to_json(orient="records", lines=True))
 
 
 def load(method="recursive-split"):
@@ -268,7 +303,8 @@ def load(method="recursive-split"):
         print(f"Collection '{collection_name}' did not exist. Creating new.")
 
     collection = client.create_collection(
-        name=collection_name, metadata={"hnsw:space": "cosine"})
+        name=collection_name, metadata={"hnsw:space": "cosine"}
+    )
     print(f"Created new empty collection '{collection_name}'")
     print("Collection:", collection)
 
@@ -289,22 +325,21 @@ def load(method="recursive-split"):
         load_text_embeddings(data_df, collection)
 
 
-def preprocess_files(method='recursive-split'):
-    print('Chunking files...')
+def preprocess_files(method="recursive-split"):
+    print("Chunking files...")
     chunk(method)
-    print('Done chunking files.')
+    print("Done chunking files.")
 
-    print('Embedding files...')
+    print("Embedding files...")
     embed(method)
-    print('Done embedding files.')
+    print("Done embedding files.")
 
-    print('Loading embeddings to vector db...')
+    print("Loading embeddings to vector db...")
     load(method)
-    print('Done loading embeddings.')
+    print("Done loading embeddings.")
 
 
 def query(user, prompt, search_string=" ", method="recursive-split"):
-
     collection = get_collection(method)
 
     query_embedding = generate_query_embedding(prompt)
@@ -317,7 +352,7 @@ def query(user, prompt, search_string=" ", method="recursive-split"):
         query_embeddings=[query_embedding],
         n_results=10,
         where={"source": "Apple Health"},
-        where_document={"$contains": search_string}
+        where_document={"$contains": search_string},
     )
     print("Query:", prompt)
     print("\n\nResults:", results)
@@ -326,11 +361,16 @@ def query(user, prompt, search_string=" ", method="recursive-split"):
 
 
 def get_relevant_search_string(prompt):
-
-    INPUT_PROMPT = f"""
-    Analyze the below text, and return the most important text string you believe is needed to get data that is the most relevant for the sentence. Do not return anything else other than that string. Do not return any other data format than a string. If you do not believe there is any substring that is particularly relevant, then answer me with "no relevant search string", but only do this if you truly do not believe there is any important substring in the text. Here is the text:
-    {prompt}
-    """
+    INPUT_PROMPT = (
+        f"Analyze the below text, and return the most important text "
+        f"string you believe is needed to get data that is the most "
+        f"relevant for the sentence. Do not return anything else other "
+        f"than that string. Do not return any other data format than a "
+        f"string. If you do not believe there is any substring that is "
+        f'particularly relevant, then answer me with "no relevant search '
+        f'string", but only do this if you truly do not believe there is '
+        f"any important substring in the text. Here is the text:\n{prompt}"
+    )
     print("INPUT_PROMPT: ", INPUT_PROMPT)
     response = generative_model.generate_content(
         [INPUT_PROMPT],
@@ -340,7 +380,7 @@ def get_relevant_search_string(prompt):
     generated_text = response.text
     print("LLM Response:", generated_text)
 
-    generated_text = generated_text.replace('"', '').strip()
+    generated_text = generated_text.replace('"', "").strip()
 
     if generated_text == "no relevant search string":
         generated_text = " "
@@ -361,10 +401,7 @@ def chat(user, prompt, method="recursive-split"):
 
     print(len(results["documents"][0]))
 
-    INPUT_PROMPT = f"""
-	{prompt}
-    {results['documents'][0]}
-    """
+    INPUT_PROMPT = f"""{prompt} {results['documents'][0]}"""
 
     print("INPUT_PROMPT: ", INPUT_PROMPT)
     response = generative_model.generate_content(
@@ -377,14 +414,10 @@ def chat(user, prompt, method="recursive-split"):
 
 
 def get(user, method="recursive-split"):
-
     collection = get_collection(method)
 
     # Get documents with filters
-    results = collection.get(
-        where={"user": user},
-        limit=1
-    )
+    results = collection.get(where={"user": user}, limit=1)
     print("\n\nResults:", results)
 
 
@@ -400,7 +433,8 @@ def agent(user, prompt, method="recursive-split"):
         ],
     )
 
-    # Step 1: Prompt LLM to find the tool(s) to execute to find the relevant chunks in vector db
+    # Step 1: Prompt LLM to find the tool(s) to execute to find
+    # the relevant chunks in vector db
     print("user_prompt_content: ", user_prompt_content)
     response = generative_model.generate_content(
         user_prompt_content,
@@ -412,16 +446,19 @@ def agent(user, prompt, method="recursive-split"):
             function_calling_config=ToolConfig.FunctionCallingConfig(
                 # ANY mode forces the model to predict only function calls
                 mode=ToolConfig.FunctionCallingConfig.Mode.ANY,
-            ))
+            )
+        ),
     )
     print("LLM Response:", response)
 
-    # Step 2: Execute the function and send chunks back to LLM to answer get the final response
+    # Step 2: Execute the function and send chunks back to LLM to answer
+    # get the final response
     function_calls = response.candidates[0].function_calls
     print("Function calls:")
     print(function_calls)
     function_responses = rag_agent_tools.execute_function_calls(
-        function_calls, user, collection, embed_func=generate_query_embedding)
+        function_calls, user, collection, embed_func=generate_query_embedding
+    )
     if len(function_responses) == 0:
         print("Function calls did not result in any responses...")
     else:
@@ -430,9 +467,7 @@ def agent(user, prompt, method="recursive-split"):
             [
                 user_prompt_content,  # User prompt
                 response.candidates[0].content,  # Function call response
-                Content(
-                    parts=function_responses
-                ),
+                Content(parts=function_responses),
             ],
             tools=[rag_agent_tools.fitness_expert_tool],
         )
@@ -452,27 +487,16 @@ def main(args=None):
         load(method=args.chunk_type)
 
     if args.query:
-        query(
-            user=args.user,
-            prompt=args.prompt,
-            method=args.chunk_type)
+        query(user=args.user, prompt=args.prompt, method=args.chunk_type)
 
     if args.chat:
-        chat(
-            user=args.user,
-            prompt=args.prompt,
-            method=args.chunk_type)
+        chat(user=args.user, prompt=args.prompt, method=args.chunk_type)
 
     if args.get:
-        get(
-            user=args.user,
-            method=args.chunk_type)
+        get(user=args.user, method=args.chunk_type)
 
     if args.agent:
-        agent(
-            user=args.user,
-            prompt=args.prompt,
-            method=args.chunk_type)
+        agent(user=args.user, prompt=args.prompt, method=args.chunk_type)
     if args.preprocess:
         preprocess_files(method=args.chunk_type)
 
@@ -522,17 +546,15 @@ if __name__ == "__main__":
         action="store_true",
         help="Preprocess txt files by chunking and embedding the content",
     )
-    parser.add_argument("--chunk_type", default="recursive-split",
-                        help="recursive-split | semantic-split"
-                        )
     parser.add_argument(
-        "--user", default="",
-        help="Str with the name of username"
+        "--chunk_type",
+        default="recursive-split",
+        help="recursive-split | semantic-split",
     )
-    parser.add_argument(
-        "--prompt", default="",
-        help="Text prompt to pass to RAG model"
-    )
+    parser.add_argument("--user", default="",
+                        help="Str with the name of username")
+    parser.add_argument("--prompt", default="",
+                        help="Text prompt to pass to RAG model")
 
     args = parser.parse_args()
 
