cec7441ecc7ff72f5827da6b6c754f016bffe156
pappoja <jakepappo@college.harvard.edu>
2024-11-16 16:31:42 -0500

    Updated frontend_react so it runs, but still need to set up with backend

--- src/frontend_react/docker-shell.sh
+++ src/frontend_react/docker-shell.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-export IMAGE_NAME="gain-frontend-react"
+export IMAGE_NAME="cheese-app-frontend-react"
 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile.dev .
 
 # Run the container
-docker run --rm --name $IMAGE_NAME -ti \
-  -v "$(pwd)/:/app/" \
-  -p 3000:3000 \
-  --network gain-rag-network \
-  $IMAGE_NAME
-
-# docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3000:3000 $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3000:3000 $IMAGE_NAME
