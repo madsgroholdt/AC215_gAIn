ca313f8285ced6b58e44bdff6136ca2e4f38bcbb
pappoja <jakepappo@college.harvard.edu>
2024-11-15 21:47:12 -0500

    Updated api_service to run locally and connect with chromadb

--- src/api_service/api/service.py
+++ src/api_service/api/service.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
-from api.routers import llm_chat, llm_cnn_chat, llm_rag_chat, llm_agent_chat
-from api.routers import newsletter, podcast
+from api.routers import llm_rag_chat
 
 # Setup FastAPI app
 app = FastAPI(title="API Server", description="API Server", version="v1")
@@ -20,6 +20,11 @@ app.add_middleware(
 @app.get("/")
 async def get_index():
     return {"message": "Welcome... to gAIn!"}
+
+# Serve the favicon
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 # Additional routers here
 app.include_router(llm_rag_chat.router, prefix="/llm-rag")
