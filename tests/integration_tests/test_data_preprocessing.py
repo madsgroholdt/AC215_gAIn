ce66ab43823295e9c67da13dcfd23112d6ea2355
Mads Groeholdt <madsgroeholdt@college.harvard.edu>
2024-11-20 09:08:14 -0500

    Changed hosting port for integration testing

--- tests/integration_tests/test_data_preprocessing.py
+++ tests/integration_tests/test_data_preprocessing.py
@@ -1,8 +1,9 @@
 # import pytest
+# from src.model_finetuner.cli import chat
 
 print('The test file is being run!!')
 
 
 def test_function():
     print('The test function is being run!!')
-    assert True
+    # assert chat() == None
