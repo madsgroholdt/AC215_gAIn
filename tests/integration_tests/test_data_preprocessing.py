ff49763574c228ddad77518cd9f8715b8cb7d9e7
Mads Groeholdt <madsgroeholdt@college.harvard.edu>
2024-11-20 14:00:03 -0500

    Minor CI changes

--- tests/integration_tests/test_data_preprocessing.py
+++ tests/integration_tests/test_data_preprocessing.py
@@ -1,9 +1,16 @@
 # import pytest
 # from src.model_finetuner.cli import chat
+import requests
 
 print('The test file is being run!!')
 
 
 def test_function():
-    print('The test function is being run!!')
-    # assert chat() == None
+    url = "http://0.0.0.0:5003/llm-rag/chats/38d61d80-bbf9-4849-a7e9-f4decacc279d"
+    headers = {
+        "X-test-session": "test-session"
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    print(response.json())
