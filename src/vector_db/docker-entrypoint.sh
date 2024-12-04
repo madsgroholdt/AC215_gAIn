a452c4bc0a0e6c8fe18d7013f2b8a2aa649a626e
pappoja <jakepappo@college.harvard.edu>
2024-12-03 20:00:28 -0500

    Changed entrypoint for vector_db so it automatically runs cli.py

--- src/vector_db/docker-entrypoint.sh
+++ src/vector_db/docker-entrypoint.sh
@@ -3,9 +3,10 @@ echo "Container is running!!!"
 args="$@"
 echo $args
 
-if [[ -z ${args} ]];
-then
-    pipenv shell
+if [[ -z ${args} ]]; then
+    echo "Running 'python cli.py --preprocess'..."
+    pipenv run python cli.py --preprocess
 else
-  pipenv run python $args
+    echo "Running custom command: $args"
+    pipenv run python $args
 fi
