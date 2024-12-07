6e6097dcc973ee86e874698c4f5be253704c8c42
pappoja <jakepappo@college.harvard.edu>
2024-12-07 02:02:07 -0500

    Updated code to deploy containers on VM instance

--- src/deployment/docker-shell.sh
+++ src/deployment/docker-shell.sh
@@ -4,10 +4,10 @@
 #set -e
 
 # Define some environment variables
-export IMAGE_NAME="cheese-app-deployment"
+export IMAGE_NAME="gain-deployment"
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../../../secrets/
-export GCP_PROJECT="ac215-project" # Change to your GCP Project
+export GCP_PROJECT="ac215-final-project"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS=/secrets/deployment.json
 
@@ -21,9 +21,9 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -v "$HOME/.ssh":/home/app/.ssh \
--v "$BASE_DIR/../api-service":/api-service \
--v "$BASE_DIR/../frontend-react":/frontend-react \
--v "$BASE_DIR/../vector-db":/vector-db \
+-v "$BASE_DIR/../api_service":/api_service \
+-v "$BASE_DIR/../frontend_react":/frontend_react \
+-v "$BASE_DIR/../vector_db":/vector_db \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e USE_GKE_GCLOUD_AUTH_PLUGIN=True \
 -e GCP_PROJECT=$GCP_PROJECT \
